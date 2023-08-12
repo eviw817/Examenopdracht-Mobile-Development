43868d60d1e1c78c0b70885e8bb46034b1511838
eviw817 <116573280+eviw817@users.noreply.github.com>
2023-08-12 13:32:17 +0200

    rename a bunch

--- my-app/App.js
+++ my-app/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './components/homepage';
-import InformationScreen from './components/information';
-import LikesScreen from './components/likes';
-import BasketScreen from './components/basket';
+import HomeScreen from './screens/homepage';
+import InformationScreen from './screens/information';
+import LikesScreen from './screens/likes';
+import BasketScreen from './screens/basket';
 
 const Stack = createStackNavigator();
 
